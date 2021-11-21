81a824080ef7eb48968d823559c3c43263a07c8c
Anri Gogiberidze <anri45gogiberidze@gmail.com>
2021-11-21 23:36:33 +0400

    Made middle coins spin

--- app.js
+++ app.js
@@ -7,3 +7,22 @@ function showMenu() {
         optionsMenu.classList.add('hidden');
     }
 }
+
+const spinningCoinLeft = document.querySelector('#rotatingCoinLeft');
+const spinningCoinRight = document.querySelector('#rotatingCoinRight');
+
+function rotateCoin() {
+    if(!spinningCoinLeft.classList.contains('rotateClass')) {
+        spinningCoinLeft.classList.add('rotateClass');
+    } else {
+        spinningCoinLeft.classList.remove('rotateClass');
+    }
+
+    if(!spinningCoinRight.classList.contains('rotateClass')) {
+        spinningCoinRight.classList.add('rotateClass');
+    } else {
+        spinningCoinRight.classList.remove('rotateClass');
+    }
+}
+
+let rotatingCoinInterval = setInterval(rotateCoin, 500);
